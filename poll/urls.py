7cf27418acffe74728ecb7ae18e9d1c4c796fe52
Samarth Singh <77071619+samarth-sudo@users.noreply.github.com>
2021-07-10 14:48:49 +0530

    Add files via upload

---
+++ poll/urls.py
@@ -0,0 +1,14 @@
+"""poll URL Configuration
+
+The `urlpatterns` list routes URLs to views. """
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', views.home, name='home'),
+    path('polls/', include('polls.urls', namespace="polls")),
+    path('accounts/', include('accounts.urls', namespace="accounts")),
+]
